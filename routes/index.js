7759af2bdd7eae6105162d5bc9a1f4b5933f28a4
Godwin Idemudia <godwinide@gmail.com>
2021-12-04 13:04:23 +0100

    new

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,9 @@ router.get("/edit-user/:id", ensureAuthenticated, async (req,res) => {
 router.post("/edit-user/:id", ensureAuthenticated, async (req,res) => {
     try{
         const {id} = req.params;
-        const {balance, pending, total_deposit, active_deposit, total_withdraw, debt, account_plan} = req.body;
+        const {balance, pending, total_deposit, total_earned, active_deposit, total_withdraw, debt, account_plan} = req.body;
         const customer = await User.findOne({_id:id}).limit(10)
-        if(!balance || !pending || !total_deposit || !active_deposit || !total_withdraw || !debt || !account_plan){
+        if(!balance || !total_earned || !pending || !total_deposit || !active_deposit || !total_withdraw || !debt || !account_plan){
             req.flash("error_msg", "Please fill all fields");
             return res.render("editUser", {pageTitle: "Welcome", customer, req});
         }
